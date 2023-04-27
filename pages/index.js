48ad6ee0f52e36310a77725cc77a19f9a4326121
Nightsroud <nescudero57@gmail.com>
2023-04-27 19:02:56 -0400

    feat: added icon button and box display

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,29 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import Grid2 from '@mui/material/Unstable_Grid2';
+import { IconButton } from '@mui/material';
+import { Button } from '@mui/material';
+import { Settings } from '@mui/icons-material';
+import { Box } from '@mui/material';
 
 export default function Index() {
   return (
-    <Grid2 container spacing={2}>
+    <Box display="flex"
+    justifyContent="center"
+    alignSelf="center"
+    >
+      <Grid2 container spacing={2} >
 
-      <Grid2 xs display="flex" justifyContent="center" alignItems="center">
-        text
+      <Grid2 xs={8} display="flex" justifyContent="right" alignItems="right">
+        </Grid2>
+
+        <Grid2 xs={4} display="flex" justifyContent="right" alignItems="right">
+          <IconButton>
+            <Settings/>
+          </IconButton>
+        </Grid2>
+
       </Grid2>
-
-    </Grid2>
+    </Box>
   )
 }
